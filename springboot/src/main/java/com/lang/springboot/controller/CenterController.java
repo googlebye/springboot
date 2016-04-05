c257215e77d3972688405ee49a3c5847fcb6f1a3
Lang <Lang@Lang-PC>
2016-04-05 20:11:21 +0800

    提交测试代码

---
+++ springboot/src/main/java/com/lang/springboot/controller/CenterController.java
@@ -0,0 +1,27 @@
+package com.lang.springboot.controller;
+
+import java.awt.print.Pageable;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lang.springboot.constants.MediaTypes;
+import com.lang.springboot.dto.InfoDto;
+
+@RestController
+public class CenterController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "hello world";
+    }
+
+    @RequestMapping(value = "/api/books", produces = MediaTypes.JSON_UTF_8)
+    public InfoDto listAllBook(Pageable pageable) {
+
+        return null;
+    }
+
+}
---
+++ springboot/src/main/java/com/lang/springboot/dto/InfoDto.java
@@ -0,0 +1,6 @@
+package com.lang.springboot.dto;
+
+
+public class InfoDto {
+
+}
